cf4a3cf2cdc4807380ad1b3025aa7512897e917a
Steven Wright <steven.wright@bigrentz.com>
2022-05-18 15:53:58 +0000

    complete the jsx ternary null to false

--- js/react-jsx-ternary-null-to-false/index.js
+++ js/react-jsx-ternary-null-to-false/index.js
@@ -1,1 +1,29 @@
+module.exports = (file, api, options) => {
+  const j = api.jscodeshift;
+  const printOptions = options.printOptions || { quote: "single" };
+  const root = j(file.source);
 
+  root
+    .find(j.ConditionalExpression)
+    .filter(
+      (path) =>
+        path.value.consequent.type === "JSXElement" ||
+        path.value.alternate.type === "JSXElement"
+    )
+    .forEach((path) => {
+      console.log(path);
+      if (
+        path.value.alternate.type === "Literal" &&
+        path.value.alternate.value === null
+      ) {
+        path.value.alternate.value = false;
+      } else if (
+        path.value.consequent.type === "Literal" &&
+        path.value.consequent.value === null
+      ) {
+        path.value.consequent.value = false;
+      }
+    });
+
+  return root.toSource(printOptions);
+};
