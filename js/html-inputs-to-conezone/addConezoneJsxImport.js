67477e97fb46c644a067edad5704cb83933e92ef
Steven Wright <steven.wright@bigrentz.com>
2022-05-26 20:23:29 +0000

    rename and cleanup work

--- js/html-inputs-to-conezone/addConezoneJsxImport.js
+++ js/html-inputs-to-conezone/addConezoneJsxImport.js
@@ -1,20 +1,20 @@
 export default function (api, root, identifierName, importName) {
   const j = api.jscodeshift
-  const conezoneImportDeclarations = root
+  const importDeclarations = root
     .find(j.ImportDeclaration)
     .filter((path) => path.node.source.value === importName);
 
-  if (conezoneImportDeclarations.length === 0) {
-    // Add the new bigrentz/conezone import specifier
-    const czInputTextImport = j.importDeclaration(
+  if (importDeclarations.length === 0) {
+    // Add the new import specifier
+    const componentImport = j.importDeclaration(
       [j.importSpecifier(j.identifier(identifierName))],
       j.stringLiteral(importName)
     );
-    root.get().node.program.body.unshift(czInputTextImport);
+    root.get().node.program.body.unshift(componentImport);
   } else {
-    // Insert the bigrentz/conezone import specifier to the existing list
+    // Insert the import specifier to the existing list
     const importSpecifier = j.importSpecifier(j.identifier(identifierName));
-    conezoneImportDeclarations.forEach((importDeclaration) =>
+    importDeclarations.forEach((importDeclaration) =>
       j(importDeclaration).replaceWith(
         j.importDeclaration(
           [...importDeclaration.node.specifiers, importSpecifier],
